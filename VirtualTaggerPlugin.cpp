7811e08a68b57356bb65b4055c059ad597e94598
Bryden Frizzell <bryden.frizzell@gmail.com>
2023-03-22 21:34:49 +1300

    Significantly improved everything - vtable entries are now marked as data - x-refs now go from vtable slot -> function - pure virtual functions now have x-refs - strings in vtable entries are removed

--- VirtualTaggerPlugin.cpp
+++ VirtualTaggerPlugin.cpp
@@ -4,6 +4,7 @@
 
 #include <qapplication.h>
 #include <qclipboard.h>
+#include <qelapsedtimer.h>
 
 #include <fstream>
 
@@ -21,53 +22,117 @@ static void updateClassNames()
     std::lock_guard<std::mutex> lock(mutex);
 
     auto core = Core();
+    auto rizinCore = core->core();
+
+    QElapsedTimer timer;
+    timer.start();
+
+    // core->getArchBits() seems to return byte size of pointer? Probably a bug?
+    // Rizin gives the correct size, so I'm using that in case Cutter's API changes
+    size_t pointerSize = rz_bin_object_get_info(rz_bin_cur_object(rizinCore->bin))->bits / 8;
+    //core->message("Bits: " + QString::number(core->getArchBits()) + " bytes: " + QString::number(pointerSize));
     auto classNames = core->getAllAnalysisClasses(false);
     core->message("VirtualTaggerPlugin: " + QString::number(classNames.size()) + " classes");
 
+    size_t methodDefinitionsCreated = 0;
+    size_t stringsRemoved = 0;
+    size_t vtableXrefsCreated = 0;
+
     for (auto className : classNames)
     {
-        // TODO multiple use
         // update name
         auto methods = core->getAnalysisClassMethods(className);
         auto baseClasses = core->getAnalysisClassBaseClasses(className);
-
-        methodloop:
-        for (auto method : methods)
+        auto classVTables = core->getAnalysisClassVTables(className);
+        if (classVTables.size() > 0)
         {
-            if (core->functionAt(method.addr) == nullptr)
+            if (classVTables[0].addr % pointerSize != 0)
+                core->message("VirtualTaggerPlugin: Bad vtable address for " + className);
+
+            // Used to find empty slots without entries
+            size_t nextOffset = classVTables[0].addr;
+
+            methodloop:
+            for (auto method : methods)
             {
-                // TODO should this be realName?
-                QString funcName = ("vmt." + className + "." + method.name);
+                // pure virtual methods (_purecall) don't have entries, this loop fills
+                // in any missing entries + adds the current method
+                for (size_t vtableEntryAddr = nextOffset; vtableEntryAddr <= classVTables[0].addr + method.vtableOffset;
+                    vtableEntryAddr += pointerSize)
+                {
+                    // Clean up vtable entry
+                    // Cutter often misinterprets vtable entries as strings, 
+                    // which also messes up Cutter's interpretation after setting to data
+                    for (int j = 0; j < pointerSize; ++j)
+                    {
+                        size_t addr = vtableEntryAddr + j;
+                        core->removeString(addr);
+                        // cleanup - clear string flags
+                        QStringList flags = core->listFlagsAsStringAt(addr).split(",");;
+                        for (auto flag : flags)
+                        {
+                            if (flag.startsWith("str."))
+                            {
+                                // HACK I think the code here clears using the *non-unique* name,
+                                // so this might destroy all definitions of the flag...
+                                // HOWEVER, String defintions in Strings panel still seem to exist.
+                                // Unfortunately, hitting F5 in the strings panel after this freezes 
+                                // Cutter... searches work though?
+                                core->delFlag(flag);
+                                ++stringsRemoved;
+                            }
+                        }
+                    }
+                    core->setToData(vtableEntryAddr, pointerSize);
 
-                // add X-ref to vtable
-                auto classVTables = core->getAnalysisClassVTables(className);
-                if (classVTables.size() > 0)
-                    // TODO: this refs the VMT START, not the SLOT of the actual function
-                    rz_analysis_xrefs_set(core->core()->analysis, classVTables[0].addr, method.addr, RzAnalysisXRefType::RZ_ANALYSIS_XREF_TYPE_DATA);
+                    // add X-ref to vtable entry
+                    rz_analysis_xrefs_set(rizinCore->analysis, vtableEntryAddr, method.addr, RzAnalysisXRefType::RZ_ANALYSIS_XREF_TYPE_DATA);
+                    ++vtableXrefsCreated;
+                     
+                    // add comment to make reading x-refs easier
+                    // Cutter's API is slow, so we set the comment directly via Rizin (sketchy but works since we run early)
+                    //core->setComment(vtableEntryAddr, className + "." + method.name);
+                    rz_meta_set_string(rizinCore->analysis, RZ_META_TYPE_COMMENT, vtableEntryAddr, (className + "." + method.name).toStdString().c_str());
+                }
 
-                // figure out the highest-level class that defines the function
-                for (auto baseClass : baseClasses)
-                    for (auto parentMethod : core->getAnalysisClassMethods(baseClass.className))
-                        if (parentMethod.addr == method.addr)
-                            // handle by processing parent
-                            goto skip;
+                // update for next loop
+                nextOffset = classVTables[0].addr + method.vtableOffset + pointerSize;
 
-                // Using this MASSIVELY increases performance cost (single-threaded analysis...)
-                //core->createFunctionAt(method.addr, funcName);
-                // This is *also* crazy slow (single-threaded analysis...)
-                //rz_core_analysis_function_add(rizinCore, funcName.toStdString().c_str(), method.addr, false);
+                // create function definitions
+                if (core->functionAt(method.addr) == nullptr)
+                {
+                    // TODO should this be realName?
+                    QString funcName = ("vmt." + className + "." + method.name);
 
-                //char* name = (char*)(new std::string(funcName.toStdString()))->c_str();
-                RzAnalysisFunction* funcDef = rz_analysis_create_function(core->core()->analysis, funcName.toStdString().c_str(), method.addr, RzAnalysisFcnType::RZ_ANALYSIS_FCN_TYPE_ANY);
-                
-            skip:
-                ;
+                    // figure out the highest-level class that defines the function
+                    for (auto baseClass : baseClasses)
+                        for (auto parentMethod : core->getAnalysisClassMethods(baseClass.className))
+                            if (parentMethod.addr == method.addr)
+                                // handle by processing parent
+                                goto skip;
+
+                    // Using this MASSIVELY increases performance cost (single-threaded analysis...)
+                    //core->createFunctionAt(method.addr, funcName);
+                    // This is *also* crazy slow (single-threaded analysis...)
+                    //rz_core_analysis_function_add(rizinCore, funcName.toStdString().c_str(), method.addr, false);
+
+                    //char* name = (char*)(new std::string(funcName.toStdString()))->c_str();
+                    RzAnalysisFunction* funcDef = rz_analysis_create_function(rizinCore->analysis, funcName.toStdString().c_str(), method.addr, RzAnalysisFcnType::RZ_ANALYSIS_FCN_TYPE_ANY);
+                    ++methodDefinitionsCreated;
+
+                skip:
+                    ;
+                }
             }
         }
     }
 
     core->functionsChanged();
-    core->message("Finished renaming");
+    core->flagsChanged();
+    core->message("VirtualTaggerPlugin: " + QString::number(methodDefinitionsCreated) + " method definitions created");
+    core->message("VirtualTaggerPlugin: " + QString::number(stringsRemoved) + " strings removed");
+    core->message("VirtualTaggerPlugin: " + QString::number(vtableXrefsCreated) + " X-Refs created");
+    core->message("VirtualTaggerPlugin: Finished tagging in " + QString::number(timer.elapsed() / 1000.0) + "s");
 }
 
 void VirtualTaggerPlugin::setupInterface(MainWindow *main)
